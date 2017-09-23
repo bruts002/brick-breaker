057c18357aa73835b97e09cb6fe9f5d130ab83f7
Michael Brutskiy <Michael.Brutskiy@calabrio.com>
2017-09-23 12:23:20 -0500

    Adding tasks

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ import balls from './data/balls';
 
 export default class Main {
     constructor() {
-        // TODO: get size, blocks and balls from a config file based on level
+        // TODO: move all GameBoard stuff into a ./GameBoard directory
+        // TODO: create ./LevelSelector directory with all the components for selecting levels
+        //          - have a playground level where they can play with all the features 
+        // TODO: handle end game better (win&lose)
+        // TODO: create ./LevelCreator directory with all the components for user created levels
         var gbSize:Size = {width:100, height: 50};
         var gb = new GameBoard(gbSize, document.body);
         gb.init(blocks, balls);
